d07167d1a5888b120e6d967612d57aa5b81db9d1
Wai Kei So <wkso@netskope.com>
2024-10-22 16:30:44 -0700

    Update deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker build -t ep-copilot-metrics-viewer . && docker run -p 8083:80 --env-file ./.env ep-copilot-metrics-viewer
+docker container stop ep-copilot-metrics-viewer
+docker container rm ep-copilot-metrics-viewer_backup && docker rename ep-copilot-metrics-viewer ep-copilot-metrics-viewer_backup
+docker build -t ep-copilot-metrics-viewer . && docker run -p 8083:80 --env-file ./.env --name ep-copilot-metrics-viewer -d -v $(pwd)/data:/data ep-copilot-metrics-viewer 
